08563c34611d812842a5531cf8e563bb001ad831
Mateus Antonio Franceschina <mateus.franceschina@compasso.com.br>
2024-03-22 17:30:31 -0300

    more gooder

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  options = {
+    sections = {
+      lualine_a = {},
+      lualine_b = { 'tabs' },
+      lualine_c = { { 'filename', path = 2 } },
+    },
+    inactive_sections = {
+      lualine_b = {},
+    },
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {},
+}
